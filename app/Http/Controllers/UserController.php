2bf4a589413645d47e58306165dfd3a04dd03399
gomez-alejo <alejito15c@gmail.com>
2025-04-01 17:38:21 -0500

    ruta, prueba

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function saludar(){
+        return "Hola desde el controlador UserController";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Models\User;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios',[UserController::class, 'saludar']);
