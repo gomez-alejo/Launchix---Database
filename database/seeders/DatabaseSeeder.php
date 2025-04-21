ebece870752d958d5f22bc4bb0b1d6ebb62b98e2
gomez-alejo <alejito15c@gmail.com>
2025-04-21 14:43:49 -0500

    xd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
-        /*$this->call(UserSeeder::class);*/
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(10)->create();
+        Category::factory(10)->create();
     }
 }
