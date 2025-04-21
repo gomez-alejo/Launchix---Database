cb842f09ab24efabde14b5b62d76d5fe3b751fce
Guillermo Gomez <cducmc@gmail.com>
2025-04-21 14:37:21 -0500

    ajuste el error

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,7 +18,7 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => $this->faker->sentence(), 
-            'type'=>$this->faker->paragraph() 
+            'type'=>$this->faker->text() 
         ];
     }
 }
