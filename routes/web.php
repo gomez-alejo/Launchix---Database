dbc16f5b8e9cfaea039aa78e618273c91fa199f2
gomez-alejo <alejito15c@gmail.com>
2025-04-22 17:36:44 -0500

    ajuste en las rutas

--- routes/web.php
+++ routes/web.php
@@ -7,28 +7,13 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
+//alejandro
 Route::get('/Launchix', function () {return view('inicio');})->name('inicio');
 Route::get('/Products', function () {return view('products');})->name('productos');
-
-
-
-
-
 Route::get('/usuario',[UserController::class,'usuario'])->name('usuario');
-
-Route::get('/productos', function () {
-    return view('productos');
-})->name('productos');
-
+Route::get('/productos', function () {return view('productos');})->name('productos');
 Route::get('/registro', [UserController::class, 'create'])->name('users.create');
 Route::post('/registro', [UserController::class, 'store'])->name('users.store');
-
-
-
-
-
-
-
 Route::get('/consulta5', [OrmController::class, 'consulta5']);
 Route::get('/prueba', [UserController::class, 'prueba'])->name('users.prueba');
 Route::get('/categories', [CategoryController::class, 'consulta'])->name('categories.consulta');
