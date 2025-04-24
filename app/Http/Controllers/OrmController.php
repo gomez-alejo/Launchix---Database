893fdcee4cfa45fddf4bb72f27ff5f3715b281a9
Sebastian Manzano <jose.eustacio.eliberto@gmail.com>
2025-04-23 21:15:22 -0500

    Consultas terminadas y aprovadas

--- app/Http/Controllers/OrmController.php
+++ app/Http/Controllers/OrmController.php
@@ -2,7 +2,7 @@
 namespace App\Models;
 namespace App\Http\Controllers;
 
-
+use App\Models\Category;
 use App\Models\Entrepreneur;
 use App\Models\Order;
 use App\Models\OrderProduct;
@@ -21,87 +21,87 @@ class OrmController extends Controller
     // Consultas individuales de cada tabla//
     /////////////////////////////////////////
 
-    //Mostrar todos los productos
-    public function consulta1(){
-        $products = Product::all();
+        //Mostrar todos los productos
+        public function consulta1(){
+            $products = Product::all();
 
-        return $products;
-    }
+            return $products;
+        }
 
-    //Mostrar todos los servicios
-    public function consulta2(){
-        $services = Service::all();
+        //Mostrar todos los servicios
+        public function consulta2(){
+            $services = Service::all();
 
-        return $services;
-    }
+            return $services;
+        }
 
-    //Mostrar todos los usuarios
-    public function consulta3(){
-        $users = User::all();
+        //Mostrar todos los usuarios
+        public function consulta3(){
+            $users = User::all();
 
-        return $users;
-    }
+            return $users;
+        }
 
-    //Mostrar todos los pagos
-    public function consulta4(){
-        $payments = Payment  ::all();
+        //Mostrar todos los pagos
+        public function consulta4(){
+            $payments = Payment  ::all();
 
-        return $payments;
-    }
+            return $payments;
+        }
 
-    //Mostrar todas las pedidos
-    public function consulta5(){
-        
-        $orders = Order  ::all();
+        //Mostrar todas las pedidos
+        public function consulta5(){
+            
+            $orders = Order  ::all();
 
-        return $orders;
-    }
+            return $orders;
+        }
 
-    //Mostrar todos los envios
-    public function consulta6(){
-        $shipments = Shipment  ::all();
+        //Mostrar todos los envios
+        public function consulta6(){
+            $shipments = Shipment  ::all();
 
-        return $shipments;
-    }
+            return $shipments;
+        }
 
-    //Mostrar todas las reseñas
-    public function consulta7(){
-        
-        $reviews = Review  ::all();
+        //Mostrar todas las reseñas
+        public function consulta7(){
+            
+            $reviews = Review  ::all();
 
-        return $reviews;
-    }
+            return $reviews;
+        }
 
-    //Mostrar todas las categorias
-    public function consulta8(){
-        
-        $categories = Review  ::all();
+        //Mostrar todas las categorias
+        public function consulta8(){
+            
+            $categories = Category  ::all();
 
-        return $categories;
-    }
+            return $categories;
+        }
 
-    //Mostrar todos los emprendedores 
-    public function consulta9(){
-        
-        $entrepreneurs = Entrepreneur  ::all();
+        //Mostrar todos los emprendedores 
+        public function consulta9(){
+            
+            $entrepreneurs = Entrepreneur  ::all();
 
-        return $entrepreneurs;
-    }
+            return $entrepreneurs;
+        }
 
-    //Mostrar todos los carros de compras
-    public function consulta10(){
-        
-        $shopping_carts = ShoppingCart::all();
+        //Mostrar todos los carros de compras
+        public function consulta10(){
+            
+            $shopping_carts = ShoppingCart::all();
 
-        return $shopping_carts;
-    }
+            return $shopping_carts;
+        }
 
-    //Mostrar todo pedidos_productos
-    public function consulta11(){
-        $orderProducts = OrderProduct  ::all();
+        //Mostrar todo pedidos_productos
+        public function consulta11(){
+            $orderProducts = OrderProduct  ::all();
 
-        return $orderProducts;
-    }
+            return $orderProducts;
+        }
 
     //////////////////////////////////////////
     ////Consultas entre tablas intermedias////
@@ -120,66 +120,80 @@ class OrmController extends Controller
      ///Consultas entre tablas////
     /////////////////////////////
 
-    public function consulta14(){
-        return Product::with('orders')->get();
+    //Mostrar un Producto específico y sus Pedidos
+    public function consulta14() {
+
+    $products = Product::find(1);  
+    return $products->orders; 
     }
-    public function consulta15(){
-        
-    }
-    public function consulta16(){
-        
-    }
-    public function consulta17(){
-        
-    }
-    public function consulta18(){
-        
-    }
-    public function consulta19(){
-        
-    }
-    public function consulta20(){
-        
-    }
-    public function consulta21(){
-        
-    }
-    public function consulta22(){
-        
-    }
-    public function consulta23(){
-        
-    }
-    public function consulta24(){
-        
-    }
-    public function consulta25(){
-        
-    }
-    public function consulta26(){
-        
-    }
-    public function consulta27(){
-        
-    }
-    public function consulta28(){
-        
-    }
-    public function consulta29(){
-        
-    }
-    public function consulta30(){
-        
-    }
-    public function consulta31(){
-        
-    }
-    public function consulta32(){
-        
-    }
-    public function consulta33(){
-        
-    }
+
+// Mostrar un Servicio específico y sus Pedidos
+public function consulta15() {
+    $services = Service::find(1);  
+    return $services->orders; 
+}
+
+// Mostrar un Usuario específico y sus Pedidos
+public function consulta16() {
+
+    $users = User::find(1); 
+    return $users->orders;  
+}
+
+// Mostrar un Pago específico y su Pedido
+public function consulta17() {
     
+    $payments = Payment::find(1);
+    return $payments->orders; 
+}
+
+// Mostrar un Pedido específico y sus Productos
+public function consulta18() {
+
+    $orders = Order::find(1);  
+    return $orders->products;  
+}
+
+// Mostrar un Envío específico y su Pedido
+public function consulta19() {
+
+    $shipments = Shipment::find(1);  
+    return $shipments->orders;  
+}
+
+//  Mostrar una Reseña específica y su Producto
+public function consulta20() {
+
+    $reviews = Review::find(1); 
+    return $reviews->products;  
+}
+
+//  Mostrar una Categoría específica y sus Productos
+public function consulta21() {
+
+    $categories = Category::find(1);  
+    return $categories->products;  
+}
+
+//  Mostrar un Emprendedor específico y sus Productos
+public function consulta22() {
+
+    $entrepreneurs = Entrepreneur::find(1); 
+    return $entrepreneurs->products;  
+}
+
+//  Mostrar un Carro de Compras específico y sus Productos
+public function consulta23() {
+
+    $shopping_carts = ShoppingCart::find(1);  
+    return $shopping_carts->products;  
+}
+
+//  Mostrar un Pedido-Producto específico y su Producto
+public function consulta24() {
+
+    $order_products = OrderProduct::find(1);  
+    return $order_products->products;  
+}
     
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/usuario',[UserController::class,'usuario'])->name('usuario');
 Route::get('/productos', function () {return view('productos');})->name('productos');
 Route::get('/registro', [UserController::class, 'create'])->name('users.create');
 Route::post('/registro', [UserController::class, 'store'])->name('users.store');
-Route::get('/consulta5', [OrmController::class, 'consulta5']);
+Route::get('/consulta2', [OrmController::class, 'consulta2']);
 Route::get('/prueba', [UserController::class, 'prueba'])->name('users.prueba');
 Route::get('/categories', [CategoryController::class, 'consulta'])->name('categories.consulta');
 
